f60cca95c965ea2f37727e1dd44c4eb5ed155ab8
Cyber Knight <82527627+Cyber-Knight-Official@users.noreply.github.com>
2021-05-26 23:42:14 +0530

    Add files via upload

---
+++ Clone.sh
@@ -0,0 +1,77 @@
+clear
+#colour code
+r='\e[1;31m'
+g='\e[1;32m'
+y='\e[1;33m'
+b='\e[1;34m'
+p='\e[1;35m'
+lb='\e[1;36m'
+gr='\e[1;30m'
+wh='\e[1;37m'
+
+echo -e $g"          ████████╗░░░░░░░█████╗░██╗░░░░░░█████╗░███╗░░██╗"
+echo -e $g"          ╚══██╔══╝░░░░░░██╔══██╗██║░░░░░██╔══██╗████╗░██║"
+echo -e $g"          ░░░██║░░░█████╗██║░░╚═╝██║░░░░░██║░░██║██╔██╗██║"
+echo -e $g"          ░░░██║░░░╚════╝██║░░██╗██║░░░░░██║░░██║██║╚████║"
+echo -e $g"          ░░░██║░░░░░░░░░╚█████╔╝███████╗╚█████╔╝██║░╚███║"
+echo -e $g"          ░░░╚═╝░░░░░░░░░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚══╝"
+echo -e $wh"          ❰❰❰❰❰❰❰ Welcome To Cyber knight Official ❱❱❱❱❱❱❱"
+echo -e $b"          ================================================"
+xdg-open https://m.youtube.com/channel/UCZVC0okFmDdLlnH_SAJVvaA
+
+echo -e $r "Cyber Warriors Official Github"
+echo -e $g "Bomb-Master Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/DaVe-Smith-89/Bomb-Master
+
+echo -e $g "T-Button Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/John-kener/T-Buttons
+
+echo -e $g "T-Master Package Cloneing..."
+echo -e $y "Installing..."
+git clone http://github.com/DaVe-Smith-89/Theme-Master
+
+echo -e $g "Animation-banner  Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/DaVe-Smith-89/Animation-Banner
+
+echo -e $g "Z-Phisher  Package Cloneing..."
+echo -e $y "Installing..."
+git clone http://github.com/DaVe-Smith-89/zphisher
+
+echo -e $g "Hide-It Package Cloneing..."
+echo -e $y "Installing..."
+git clone http://github.com/john-kener/Hide-it
+
+echo -e $g "Infect Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/noob-hackers/infect
+
+echo -e $g "Evil-Spammer Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/John-kener/Evil-spammer
+
+echo -e $g "Malicious Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/John-kener/Malicious
+
+echo -e $g "Grabcam Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/noob-hackers/grabcam
+
+echo -e $g "Shl_v1 Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/John-kener/Shl_v1
+
+echo -e $r "Cyber knight Official Github"
+echo -e $g "Port-Scanner  Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/Cyber-Knight-Official/Port-Scanner-Cyber-Knight.git
+
+echo -e $r "Dark Sec"
+echo -e $g "Hidden Eye  Package Cloneing..."
+echo -e $y "Installing..."
+git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy.git
+
+bash Asci.sh
